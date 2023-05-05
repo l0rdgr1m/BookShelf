2cbb3704f52b161eccdf0959700a80a798ae36bb
lordgrim <ilia.koli@yandex.ru>
2023-05-05 11:00:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bookshelf/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.bookshelf
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.core.content.ContextCompat
+import com.example.bookshelf.ui.BooksApp
+import com.example.bookshelf.ui.theme.BookShelfTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BookShelfTheme {
+                BooksApp(
+                    onBookClicked = {
+                        ContextCompat.startActivity(
+                            this,
+                            Intent(Intent.ACTION_VIEW, Uri.parse(it.previewLink)),
+                            null
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
